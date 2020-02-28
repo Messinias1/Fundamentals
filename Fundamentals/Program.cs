bd57e884941d53057c9698dfb81be6b651f36be9
Carl Kakisis <Messinias1@gmail.com>
2020-02-27 23:39:30 -0500

    Add project files.

---
+++ Fundamentals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // book object/instance of the book class
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            Console.WriteLine(book1.title);
+        }
+    }
+}
