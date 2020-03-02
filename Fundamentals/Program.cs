a7f6ee54e169486a05b206ffb8be660b7f0fcd12
Carl Kakisis <Messinias1@gmail.com>
2020-03-01 21:49:58 -0500

    2d arrays

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -10,12 +10,16 @@ namespace Fundamentals
     {
         static void Main(string[] args)
         {
+            // 2D ARRAY INITIALIZED WITH A VALUE AT EACH INDEX
             int[,] numberGrid = { {1, 2 }, {3, 4 }, {5, 6 } };
 
+            // 2D ARRAY INITIALIZED WITH ONLY THE SIZE OF EACH DIMENSION
             int[,] myArray = new int[2, 3];
+            // ADDING AN ELEMENT AT AN INDEX
             myArray[0, 0] = 5;
             Console.WriteLine(myArray[0, 0]);
 
+            // GETTING EACH DIMENSION OF ARRAY AND LOOPING THROUGH TO VIEW EACH ELEMENT
             int uBound0 = numberGrid.GetUpperBound(0);
             int uBound1 = numberGrid.GetUpperBound(1);
             for (int i = 0; i <= uBound0; i++)
